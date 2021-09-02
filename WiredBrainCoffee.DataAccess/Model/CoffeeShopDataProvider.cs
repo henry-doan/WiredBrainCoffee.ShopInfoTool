26a35a1460ffef6bb82cea14fe331e7efe791f42
Henry Doan <henrydoan96@gmail.com>
2021-09-02 00:00:52 -0600

    program basic

---
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using WiredBrainCoffee.DataAccess.Model;
+using System.Collections.Generic;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockKg = 107 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStockKg = 107 };
+            yield return new CoffeeShop { Location = "Munich", BeansInStockKg = 107 };
+        }
+    }
+}
