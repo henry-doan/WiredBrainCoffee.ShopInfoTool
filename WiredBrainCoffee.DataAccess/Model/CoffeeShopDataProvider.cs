baf071a7e75afe93d8a9c662bc6a1bb7fe9eeff7
Henry Doan <henrydoan96@gmail.com>
2021-09-02 21:00:55 -0600

    added paper cups in stock

--- WiredBrainCoffee.DataAccess/Model/CoffeeShopDataProvider.cs
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShopDataProvider.cs
@@ -1,4 +1,4 @@
-﻿using WiredBrainCoffee.DataAccess.Model;
+using WiredBrainCoffee.DataAccess.Model;
 using System.Collections.Generic;
 
 namespace WiredBrainCoffee.DataAccess
@@ -7,9 +7,24 @@ namespace WiredBrainCoffee.DataAccess
     {
         public IEnumerable<CoffeeShop> LoadCoffeeShops()
         {
-            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockKg = 107 };
-            yield return new CoffeeShop { Location = "Freiburg", BeansInStockKg = 107 };
-            yield return new CoffeeShop { Location = "Munich", BeansInStockKg = 107 };
+            yield return new CoffeeShop 
+            { 
+                Location = "Frankfurt", 
+                BeansInStockKg = 107,
+                PaperCupsInStock = 350
+            };
+            yield return new CoffeeShop 
+            { 
+                Location = "Freiburg", 
+                BeansInStockKg = 107, 
+                PaperCupsInStock = 250
+            };
+            yield return new CoffeeShop 
+            { 
+                Location = "Munich", 
+                BeansInStockKg = 107, 
+                PaperCupsInStock = 427
+            };
         }
     }
 }
